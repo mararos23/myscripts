336cac1edff4dc8e461a8f54df45236fc7255405
mararos23 <mararos23@hotmail.com>
2023-07-16 13:02:05 +0700

    updates

--- demo.js
+++ demo.js
@@ -1,6 +1,8 @@
-// var myComp = app.project.item(1)
+var myComp = app.project.item(1)
 
-// var layerCount = myComp.numLayers
-// for (i = 1; i < layerCount + 1; i++) {
-// 	myComp.layer(i).name = 'Cosmos' + '_' + i
-// }
+var layerCount = myComp.numLayers
+for (i = 1; i < layerCount + 1; i++) {
+	myComp.layer(i).name = 'Cosmos' + '_' + i
+}
+
+//! Test github
